8c62c084f8958aa64d0bd45a63000b9b2393a6d0
Zaynah1999 <azaynah295@gmail.com>
2024-12-02 23:10:18 +0000

    Day2- solved problem (kept original code to show progress)

--- day1/removeDuplicates.py
+++ day1/removeDuplicates.py
@@ -1,9 +1,21 @@
-def removeDuplicates(nums):
-    convert = [i for n, i in enumerate(nums) if i not in nums[n+1:]]
-    expectedNums = (len(convert))
-    print(expectedNums)
-    print(convert)
+# def removeDuplicates(nums):
+#     convert = [i for n, i in enumerate(nums) if i not in nums[n+1:]]
+#     expectedNums = (len(convert))
+#     print(expectedNums)
+#     print(convert)
 
-removeDuplicates([0,0,1,1,1,2,2,3,3,4])
-removeDuplicates([1,1,2])
+# removeDuplicates([0,0,1,1,1,2,2,3,3,4])
+# removeDuplicates([1,1,2])
 
+def removeDuplicates(self, nums):
+        if not nums:
+            return 0
+        
+        itemPostion = 0
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[itemPostion]:
+                itemPostion += 1 
+                nums[itemPostion] = nums[i] 
+
+        return itemPostion + 1

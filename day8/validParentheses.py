93b4f59e9dd8d4244a56bfca6c4e193973ded0e5
Zaynah1999 <azaynah295@gmail.com>
2024-12-09 21:56:16 +0000

    day9- solved valid parentheses

--- day8/validParentheses.py
+++ day8/validParentheses.py
@@ -1,11 +1,38 @@
+# def isValid(self, s: str) -> bool:
+#         open_ = ['(', '[', '{']
+#         close_ = [')', ']','}']
+#         split_s = list(s)
+#         vals = [j in split_s for j in open_]
+#         if not any(vals) :
+#             return False
+#         for op, cl in zip(open_, close_):
+#             if op in s:
+#                 if cl not in s:
+                #     return False
+                
+
 def isValid(self, s: str) -> bool:
-        open_ = ['(', '[', '{']
-        close_ = [')', ']','}']
-        split_s = list(s)
-        vals = [j in split_s for j in open_]
-        if not any(vals) :
+        # open_ = ['(', '[', '{']
+        # close_ = [')', ']','}']
+        # split_s = list(s)
+        # vals = [j in split_s for j in open_]
+        # if not any(vals) :
+        #     return False
+        # for op, cl in zip(open_, close_):
+        #     if op in s:
+        #         if cl not in s:
+        #             return False
+        valid = True
+        while valid:
+            if s == '':
+                return True
+            if '()' in s:
+                s = s.replace("()", "")
+                continue
+            if '{}' in s:
+                s = s.replace("{}", "")
+                continue
+            if '[]' in s:
+                s = s.replace("[]", "")
+                continue
             return False
-        for op, cl in zip(open_, close_):
-            if op in s:
-                if cl not in s:
-                    return False
